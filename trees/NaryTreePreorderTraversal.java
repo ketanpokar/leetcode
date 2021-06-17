9211420e1f76aa13b9b772f19765b1f97b75edc9
Ketan Pokar <31739284+ketanpokar@users.noreply.github.com>
2021-06-17 22:20:53 +0530

    589. N-ary Tree Preorder Traversal

---
+++ trees/NaryTreePreorderTraversal.java
@@ -0,0 +1,35 @@
+// Problem Link : https://leetcode.com/problems/n-ary-tree-preorder-traversal/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class NaryTreePreorderTraversal {
+    public List<Integer> preorder(Node root) {
+        
+        if(root == null)
+            return new ArrayList<>();
+        
+        List<Integer> values = new ArrayList<>();
+        values.add(root.val);
+        for(Node node : root.children) {
+            values.addAll(preorder(node));
+        }
+        return values;
+    }
+}
