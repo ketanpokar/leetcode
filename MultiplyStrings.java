b9a2476fa9c5fb39c2eda4af1a42305767079c8a
Ketan Pokar <31739284+ketanpokar@users.noreply.github.com>
2021-06-23 22:03:52 +0530

    43. Multiply Strings

---
+++ MultiplyStrings.java
@@ -0,0 +1,8 @@
+// Problem Link : https://leetcode.com/problems/multiply-strings/
+
+import java.math.BigInteger;
+class MultiplyStrings {
+    public String multiply(String num1, String num2) {
+        return new BigInteger(num1).multiply(new BigInteger(num2)).toString();
+    }
+}
